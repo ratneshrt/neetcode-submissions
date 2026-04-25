3d1fdec20e16b051cb03c9186799138bb9d13f34
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2026-04-25 23:53:31 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.go
@@ -0,0 +1,40 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Neighbors []*Node
+ * }
+ */
+
+func cloneGraph(node *Node) *Node {
+    if node == nil{
+        return nil
+    }
+
+    stored := make(map[*Node]*Node)
+
+    return dfs(node, stored)
+}
+
+func dfs(node *Node, stored map[*Node]*Node) *Node {
+    if node == nil {
+        return node
+    }
+
+    if clone, ok := stored[node];ok {
+        return clone
+    }
+
+    clone := &Node{
+        Val: node.Val,
+        Neighbors: []*Node{},
+    }
+
+    stored[node] = clone
+
+    for _, neigh := range node.Neighbors {
+        clone.Neighbors = append(clone.Neighbors, dfs(neigh, stored))
+    }
+
+    return clone
+}
