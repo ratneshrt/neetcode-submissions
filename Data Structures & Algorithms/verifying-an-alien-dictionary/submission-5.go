56efc15f655bcbac15f1138d1feb5efcead9a34b
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2026-04-27 11:56:24 +0530

    Add: verifying-an-alien-dictionary - submission-5

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-5.go
@@ -0,0 +1,29 @@
+func isAlienSorted(words []string, order string) bool {
+    rank := make(map[byte]int)
+
+    for i := 0; i<len(order);i++{
+        rank[order[i]] = i
+    }
+
+    for i:=0;i<len(words)-1;i++{
+        word1 := words[i]
+        word2 := words[i+1]
+
+        foundDiff := true
+        for j:=0;j<min(len(word1), len(word2));j++{
+            if word1[j] != word2[j]{
+                if rank[word1[j]] > rank[word2[j]]{
+                    return false
+                }
+                foundDiff = false
+                break
+            }
+        }
+
+        if foundDiff && len(word1) > len(word2){
+            return false
+        }
+    }
+
+    return true
+}
