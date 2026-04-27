5f182a569b42c96f84ddb8c73829a197df382144
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2026-04-27 11:38:21 +0530

    Add: island-perimeter - submission-3

---
+++ Data Structures & Algorithms/island-perimeter/submission-3.go
@@ -0,0 +1,20 @@
+func islandPerimeter(grid [][]int) int {
+    perimeter := 0
+    rows := len(grid)
+    cols := len(grid[0])
+    for i:=0;i<rows;i++{
+        for j:=0;j<cols;j++{
+            if grid[i][j] == 1{
+                perimeter += 4
+                if i >0 && grid[i-1][j] == 1{
+                    perimeter -=2
+                }
+                if j>0 && grid[i][j-1] == 1{
+                    perimeter -= 2
+                }
+            }
+        }
+    }
+
+    return perimeter
+}
