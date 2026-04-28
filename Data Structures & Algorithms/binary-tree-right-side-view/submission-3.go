34e9a46365623b9ff0609f475daf0de07744edf1
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2026-04-28 17:00:40 +0530

    Add: binary-tree-right-side-view - submission-3

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-3.go
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	queue := []*TreeNode{root}
+	res := []int{}
+
+	for len(queue) > 0 {
+		size := len(queue)
+
+		for i:=0; i<size;i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			if i == size - 1{
+				res = append(res, node.Val)
+			}
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	} 
+
+	return res
+}
