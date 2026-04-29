a0c0b363daa51a5b7bdfadad46a79ae9e6e642c5
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2026-04-30 01:46:10 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.go
@@ -0,0 +1,42 @@
+func canFinish(numCourses int, prerequisites [][]int) bool {
+    graph := make(map[int][]int)
+	indegree := make([]int, numCourses)
+
+	for i:=0;i<numCourses;i++{
+		graph[i] = []int{}
+	}
+
+	for _, edge := range prerequisites {
+		u := edge[0]
+		v := edge[1]
+
+		graph[u] = append(graph[u], v)
+		indegree[v]++
+	}
+
+	queue := []int{}
+
+	for i:=0;i<numCourses;i++{
+		if indegree[i] == 0 {
+			queue = append(queue, i)
+		}
+	}
+
+	processed := 0 
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		processed++
+
+		for _, neigh := range graph[node] {
+			indegree[neigh]--
+
+			if indegree[neigh] == 0{
+				queue = append(queue, neigh)
+			}
+		}
+	}
+
+	return processed == numCourses
+}
