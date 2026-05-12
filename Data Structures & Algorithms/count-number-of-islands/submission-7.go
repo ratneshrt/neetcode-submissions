ada6a9b0ab29052875da83ec6404d8578bdc381d
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2026-05-12 11:56:27 +0530

    Add: count-number-of-islands - submission-7

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-7.go
@@ -0,0 +1,42 @@
+func numIslands(grid [][]byte) int {
+	rows := len(grid)
+	cols := len(grid[0])
+	island := 0
+
+	for i:=0;i<rows;i++{
+		for j:=0;j<cols;j++{
+			if grid[i][j] == '1'{
+				island+=1
+				dfs(grid, rows, cols,i,j)
+			}
+		}
+	}
+
+	return island
+}
+
+func dfs(grid [][]byte, rows, cols, r,c int){
+	if r <0 || r>=rows || c<0 || c>=cols{
+		return
+	}
+
+	if grid[r][c] == '0'{
+		return 
+	}
+
+	grid[r][c] = '0'
+
+	directions := [][]int{
+		{0,1},
+		{0,-1},
+		{1,0},
+		{-1,0},
+	}
+
+	for _, dir := range directions {
+		dr := dir[0]
+		dc := dir[1]
+
+		dfs(grid, rows, cols, r+dr, c+dc)
+	}
+}
