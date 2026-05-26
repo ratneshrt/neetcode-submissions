6df1b4aa4b2153b97784d0564f2e0c88f354e474
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2026-05-27 02:06:58 +0530

    Add: reverse-string - submission-4

---
+++ Data Structures & Algorithms/reverse-string/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        vector<char> tmp;
+        int n = s.size();
+        for (int i=n-1;i>=0;i--){
+            tmp.push_back(s[i]);
+        }
+
+        for (int i=0;i<n;i++){
+            s[i] = tmp[i];
+        }
+
+        
+    }
+};
