51940c59abe8932c1d68e8d7aaf4ce17db1ef198
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2026-06-20 01:34:14 +0530

    Add: duplicate-integer - submission-12

---
+++ Data Structures & Algorithms/duplicate-integer/submission-12.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        int n = nums.size();
+        for (int n: nums){
+            if (seen.find(n) != seen.end()){
+                return true;
+            }
+            seen.insert(n);
+        }
+        return false;
+    }
+};
