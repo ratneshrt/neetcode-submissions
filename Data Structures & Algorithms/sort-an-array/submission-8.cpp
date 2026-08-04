5bfed04ecfa145a3b670d0503fc45ab5ba582ff7
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2026-08-05 01:40:26 +0530

    Add: sort-an-array - submission-8

---
+++ Data Structures & Algorithms/sort-an-array/submission-8.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1){
+            return nums;
+        }
+        int mid = n/2;
+        vector<int> left(nums.begin(), nums.begin() + mid);
+        vector<int> right(nums.begin() + mid, nums.end());
+
+        vector<int> sortedleft = sortArray(left);
+        vector<int> sortedright = sortArray(right);
+
+        return merge(sortedleft, sortedright);
+    }
+
+    vector<int> merge(vector<int>& left, vector<int>& right){
+        int i=0, j=0;
+        vector<int> res;
+        while(i<left.size() && j<right.size()){
+            if(left[i] < right[j]){
+                res.push_back(left[i]);
+                i++;
+            }else{
+                res.push_back(right[j]);
+                j++;
+            }
+        }
+
+        res.insert(res.end(), left.begin()+i,left.end());
+        res.insert(res.end(), right.begin()+j,right.end());
+
+        return res;
+    }
+};
