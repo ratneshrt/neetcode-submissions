0919521f0647fafb61e4c3664bd7e1c83d88cd2c
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2026-09-13 23:44:56 +0530

    Add: majority-element-ii - submission-6

---
+++ Data Structures & Algorithms/majority-element-ii/submission-6.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1 = 0;
+        int cnt2  =0;
+        int cand1 = 0;
+        int cand2 = 0;
+
+        for(int x: nums){
+            if(x == cand1){
+                cnt1++;
+            }else if(x == cand2){
+                cnt2++;
+            }else if(cnt1 == 0){
+                cand1 = x;
+                cnt1++;
+            }else if(cnt2 == 0){
+                cand2 = x;
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1= 0;
+        cnt2 = 0;
+
+        for(int x: nums){
+            if(x == cand1){
+                cnt1++;
+            } 
+
+            if(x == cand2){
+                cnt2++;
+            }
+        }
+
+        vector<int> res;
+        if(cnt1 > nums.size()/3){
+            res.push_back(cand1);
+        }
+
+        if(cnt2 > nums.size()/3){
+            res.push_back(cand2);
+        }
+
+        return res;
+    }
+};
