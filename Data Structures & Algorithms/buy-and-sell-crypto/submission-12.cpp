75d5a44dac70095acfc6db4fec00b5ffd0e76587
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2026-09-26 14:18:10 +0530

    Add: buy-and-sell-crypto - submission-12

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-12.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice = prices[0];
+        int maxprofit = 0;
+        for(int i = 1; i<prices.size();i++){
+            if(minprice > prices[i]){
+                minprice = prices[i];
+            }
+
+            int profit = prices[i] - minprice;
+            maxprofit = max(profit, maxprofit);
+        }
+        return maxprofit;
+    }
+};
